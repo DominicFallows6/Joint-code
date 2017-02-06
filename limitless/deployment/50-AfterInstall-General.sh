434aec0314fb5f23b9602edefe8fc85ac4de2c5c
James Oates <james.oates@trueshopping.co.uk>
2017-02-06 16:42:49 +0000

    Update 50-AfterInstall-General.sh

--- limitless/deployment/50-AfterInstall-General.sh
+++ limitless/deployment/50-AfterInstall-General.sh
@@ -8,4 +8,6 @@ echo "Moving configuration to directory"
 mv "${DIR}/limitless/env.php" "${DIR}/app/etc/env.php"
 
 echo "Attaching Media Mount"
-ln -s "/efs/magento2/media" "${DIR}/pub/media"
+if [ ! -L $DIR/pub/media ]; then
+  ln -s "/efs/magento2/media" "${DIR}/pub/media"
+fi
