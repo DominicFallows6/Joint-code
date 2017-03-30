03262ee894bc68bf838b6d1dc3c6d954da7c8330
Dave Tabron <david.tabron@trueshopping.co.uk>
2017-03-30 08:33:42 +0100

    Schema.org module (#78)
    * Schema Module
    
    * Code review changes
    
    * Module name change
    
    * fixing dependency issue during compile

---
+++ app/code/Limitless/SchemaOrg/Block/View.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Limitless\SchemaOrg\Block;
+
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Store\Model\StoreManagerInterface;
+
+class View extends Template
+{
+
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        array $data = []
+    ) {
+        parent::__construct($context,$data);
+        $this->registry = $registry;
+        $this->storeManager = $context->getStoreManager();
+    }
+
+    public function getProductSchema() {
+        $schemaCondition = "<meta itemprop='itemCondition' content='http://schema.org/NewCondition' />";
+        return $schemaCondition;
+    }
+
+    public function getOffersSchema() {
+
+        $finalPrice = (float) $this->registry->registry('product')->getFinalPrice();
+        $currencyCode = $this->storeManager->getStore()->getCurrentCurrencyCode();
+        $inStock = $this->registry->registry('product')->isSaleable();
+
+        $schemaPriceHtml = "<meta itemprop='price' content='$finalPrice' />";
+        $schemaCurrencyCodeHtml = "<meta itemprop='priceCurrency' content='$currencyCode' />";
+
+        if($inStock) {
+            $stockSchemaHtml = "<link itemprop='availability' href='http://schema.org/InStock' />";
+        } else {
+            $stockSchemaHtml = "<link itemprop='availability' href='http://schema.org/OutOfStock' />";
+        }
+
+        return $schemaPriceHtml . $schemaCurrencyCodeHtml . $stockSchemaHtml;
+
+    }
+}
