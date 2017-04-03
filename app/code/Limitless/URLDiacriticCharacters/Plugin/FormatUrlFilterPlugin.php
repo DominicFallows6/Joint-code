0eb33ba8b48aed672af24bda6b432622f5c3caaa
MSkelton-LDG <mark.skelton@trueshopping.co.uk>
2017-04-03 16:26:51 +0100

    url convert for special characters (#74)
    * Fix for logged in customers not able to complete order
    
    * Module to allow conversion of extra chars in URL
    
    * Module to allow conversion of extra chars in URL
    
    * Module to allow conversion of extra chars in URL composer
    
    * Changes requested by Vinai
    
    * PDF product module - config

---
+++ app/code/Limitless/URLDiacriticCharacters/Plugin/FormatUrlFilterPlugin.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Limitless\URLDiacriticCharacters\Plugin;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Filter\TranslitUrl;
+
+class FormatUrlFilterPlugin
+{
+    /** @var ScopeConfigInterface */
+    private $scopeConfig;
+
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @param TranslitUrl $subject
+     * @param string $string
+     * @return array
+     */
+    public function beforeFilter(TranslitUrl $subject, string $string)
+    {
+        $translationListString = $this->getTranslationLists();
+        if ($translationListString) {
+            $string = $this->replaceChars($string, $translationListString);
+        }
+
+        return [$string];
+    }
+
+    /**
+     * @param string $inputString
+     * @param string $translationListRaw
+     * @return mixed
+     */
+    private function replaceChars(string $inputString, string $translationListRaw)
+    {
+        $termSplit = ':';
+        $multipleSplit = ',';
+
+        $translateFrom = $translateTo = [];
+
+        $convertTerms = explode($multipleSplit, $translationListRaw);
+
+        foreach ($convertTerms as $term)
+        {
+            if (strpos($term, $termSplit) !== false)
+            {
+                $conversionParams = explode($termSplit, $term, 2);
+                if (isset($conversionParams[0]) && isset($conversionParams[1]))
+                {
+                    $translateFrom[] = trim($conversionParams[0]);
+                    $translateTo[] = trim($conversionParams[1]);
+                }
+            }
+        }
+        return str_replace($translateFrom, $translateTo, $inputString);
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getTranslationLists()
+    {
+        return $this->scopeConfig->getValue(
+            'general/limitless_url_character_convert/character_convert_list'
+        );
+    }
+
+}
