06d010a0a8c41451c69193e96b031fa8941fb147
MSkelton-LDG <mark.skelton@trueshopping.co.uk>
2017-05-05 12:16:29 +0100

    Fix for search pagination (#163)

--- app/code/Limitless/TagManagerDataLayer/Block/DataLayer/SearchDataLayer.php
+++ app/code/Limitless/TagManagerDataLayer/Block/DataLayer/SearchDataLayer.php
@@ -3,7 +3,7 @@
 namespace Limitless\TagManagerDataLayer\Block\DataLayer;
 
 use Limitless\TagManagerDataLayer\Api\DataLayerAbstract;
-use Magento\Catalog\Block\Product\ListProduct;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\View\Element\Template\Context;
 use Limitless\TagManagerDataLayer\Helper\TagsDataLayer\DynamicRemarketing;
 
@@ -18,26 +18,90 @@ class SearchDataLayer extends DataLayerAbstract
     /** @var \Magento\Catalog\Model\Product[]  */
     private $searchProducts = [];
 
-    /** @var ListProduct */
-    private $listProduct;
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
 
     public function __construct(
         Context $context,
         DynamicRemarketing $dynamicRemarketingHelper,
-        ListProduct $listProduct,
+        ProductRepositoryInterface $productRepository,
         $data = []
     ) {
         parent::__construct($context, $data);
 
         $this->dynamicRemarketingHelper = $dynamicRemarketingHelper;
-        $this->listProduct = $listProduct;
         $this->dataLayerVariables = [];
+        $this->productRepository = $productRepository;
     }
 
     public function initDataLayerVariables()
     {
-        $this->searchProducts = $this->listProduct->getLoadedProductCollection()->getItems();
+        $this->loadProducts();
         $this->initDynamicRemarketingDLVariables();
+    }
+
+    /**
+     * Assign products from product collection
+     */
+    private function loadProducts()
+    {
+        //See \Magento\Catalog\Block\Product\ListProduct _beforeToHtml()
+        $productCollectionData = $this->buildSearchPageList();
+
+        $maxProductsToShow = $this->dynamicRemarketingHelper->getMaxProductsDisplayedPublic();
+        $counter = 0;
+        $products = [];
+
+        while($counter < $maxProductsToShow) {
+
+            if (isset($productCollectionData[$counter])) {
+                $productData = $productCollectionData[$counter];
+
+                $products[] = $this->productRepository->get($productData['sku']);
+                $counter++;
+            }
+            else {
+                break;
+            }
+        }
+        $this->searchProducts = $products;
+    }
+
+    /**
+     * @return array
+     */
+    private function buildSearchPageList(): array
+    {
+        /* @see \Magento\Catalog\Block\Product\ListProduct _beforeToHtml()
+
+        /** @var \Magento\Catalog\Block\Product\ListProduct\Interceptor $searchBlock */
+        $searchBlock = $this->getLayout()->getBlock('search_result_list');
+
+        /** @var \Magento\Catalog\Block\Product\ProductList\Toolbar $toolbar */
+        $toolbar = $searchBlock->getToolbarBlock();
+        $productCollection = $searchBlock->getLoadedProductCollection();
+
+        $orders = $searchBlock->getAvailableOrders();
+        if ($orders) {
+            $toolbar->setAvailableOrders($orders);
+        }
+        $sort = $searchBlock->getSortBy();
+        if ($sort) {
+            $toolbar->setDefaultOrder($sort);
+        }
+        $dir = $searchBlock->getDefaultDirection();
+        if ($dir) {
+            $toolbar->setDefaultDirection($dir);
+        }
+        $modes = $searchBlock->getModes();
+        if ($modes) {
+            $toolbar->setModes($modes);
+        }
+
+        // set collection to toolbar and apply sort
+        $toolbar->setCollection($productCollection);
+
+        return $productCollection->getData();
     }
 
     /**
