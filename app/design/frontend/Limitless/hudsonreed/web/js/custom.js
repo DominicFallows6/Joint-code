cb4f77874bdd8d8241ef0453e7916921508d8a8d
Dave Tabron <david.tabron@trueshopping.co.uk>
2017-05-09 15:55:53 +0100

    Footer links issue on mobile devices - TP8552 (#141)
    * Footer links issue on mobile devices
    
    * removing click for footer hdrs without toggle
    
    * Mobile nav issue when titles too long
    
    * Issue with top level cat display if no sub cats on mobile
    
    * Worldpay icon width change

--- app/design/frontend/Limitless/hudsonreed/web/js/custom.js
+++ app/design/frontend/Limitless/hudsonreed/web/js/custom.js
@@ -3,19 +3,12 @@ define(['jquery'], function($) {
     "use strict";
 
     var toggle = function() {
-        if ($(window).width() < 640) {
-            $("ul.footer .hdr").click(function () {
-                $(this).toggleClass("active");
-                $(this).parent().find("li.footer-toggle").toggleClass("active");
-            });
-        }
+        $("ul.footer .hdr").click(function () {
+            $(this).toggleClass("active");
+            $(this).parent().find("li.footer-toggle").toggleClass("active");
+        });
     };
 
-    $(function(){
-        toggle();
-        $(window).on("resize", function() {
-            toggle();
-        });
-    });
+    toggle();
 
 });
