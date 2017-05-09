afa34cc4f3b2abe07e17a3263011b51aac3ae60c
James Oates <james.oates@trueshopping.co.uk>
2017-05-09 08:26:29 +0100

    Update to deployment (#168)
    * Delete env.production.php
    
    * Delete env.staging.php
    
    * Delete index.php
    
    * Update 50-AfterInstall-General.sh

--- limitless/deployment/50-AfterInstall-General.sh
+++ limitless/deployment/50-AfterInstall-General.sh
@@ -5,29 +5,9 @@ DIR=/var/www/html
 [ ! -d "$DIR/app" ] && echo "Not in Magento 2 root directory" >&2 && exit 1
 
 echo "Linking the configuration to /app/etc"
-# Deploy the local.xml file.
-case "${DEPLOYMENT_GROUP_NAME}" in
-  Staging)
-    if [ ! -L ${DIR}/app/etc/env.php ]; then
-      ln -s "${DIR}/limitless/env.staging.php" "${DIR}/app/etc/env.php"
-    fi
-    ;;
-  StagingWorker)
-    if [ ! -L ${DIR}/app/etc/env.php ]; then
-      ln -s "${DIR}/limitless/env.staging.php" "${DIR}/app/etc/env.php"
-    fi
-    ;;
-  Production)
-    if [ ! -L ${DIR}/app/etc/env.php ]; then
-      ln -s "${DIR}/limitless/env.production.php" "${DIR}/app/etc/env.php"
-    fi
-    ;;
-  ProductionWorker)
-    if [ ! -L ${DIR}/app/etc/env.php ]; then
-      ln -s "${DIR}/limitless/env.production.php" "${DIR}/app/etc/env.php"
-    fi
-    ;;
-esac
+if [ ! -L ${DIR}/app/etc/env.php ]; then
+  ln -s "/efs/magento2/env.php" "${DIR}/app/etc/env.php"
+fi
 
 echo "Attaching Media Mount"
 if [ ! -L $DIR/pub/media ]; then
