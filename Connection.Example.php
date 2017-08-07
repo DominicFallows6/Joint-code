632d3347a0fc6abbaf442c25433feea259f65eb4
Josh Campbell <josh.campbell@limitlessdigital.com>
2017-08-07 14:30:12 +0100

    Added new connection backup

--- Connection.Example.php
+++ Connection.Example.php
@@ -5,7 +5,7 @@ class Connection
     public static function make()
     {
         try {
-            return new PDO('mysql:host=127.0.0.1;dbname=dominic', 'root', 'Welcome17');
+            return new PDO('mysql:host=127.0.0.1;dbname=josh', 'root', 'Welcome17');
         } catch (PDOException $e) {
 
             die($e->getMessage());
