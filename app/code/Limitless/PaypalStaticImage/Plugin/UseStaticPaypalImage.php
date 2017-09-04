ebb316d7af136bd2d504aa2d8cc186cda1195e75
MSkelton-LDG <mark.skelton@limitlessdigital.com>
2017-09-04 14:35:35 +0100

    Add ES / IT lang buttons (#287)

--- app/code/Limitless/PaypalStaticImage/Plugin/UseStaticPaypalImage.php
+++ app/code/Limitless/PaypalStaticImage/Plugin/UseStaticPaypalImage.php
@@ -4,6 +4,7 @@ namespace Limitless\PaypalStaticImage\Plugin;
 
 use Magento\Backend\Block\Template;
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Asset\Repository;
 use Magento\Paypal\Model\Config;
 
 class UseStaticPaypalImage
@@ -16,11 +17,17 @@ class UseStaticPaypalImage
      * @var ScopeConfigInterface
      */
     private $scopeConfig;
+    /**
+     * @var Repository
+     */
+    private $assetRepository;
 
-    public function __construct(Template $template, ScopeConfigInterface $scopeConfig)
+
+    public function __construct(Template $template, ScopeConfigInterface $scopeConfig, Repository $repository)
     {
         $this->template = $template;
         $this->scopeConfig = $scopeConfig;
+        $this->assetRepository = $repository;
     }
 
     /**
@@ -40,16 +47,43 @@ class UseStaticPaypalImage
         $orderTotal = null,
         $pal = null
     ) {
+        $payPalImageViewSource = null;
+
         if ($this->getStaticPaypalImageEnabled()) {
-            return $this->getStaticImage();
-        } else {
-            return $proceed($localeCode, $orderTotal, $pal);
+            $payPalImageViewSource = $this->getStaticImage($localeCode);
         }
+
+        if (null == $payPalImageViewSource) {
+            $payPalImageViewSource = $proceed($localeCode, $orderTotal, $pal);
+        }
+
+        return $payPalImageViewSource;
     }
 
-    private function getStaticImage()
+    private function getStaticImage($locale)
     {
-        return $this->template->getViewFileUrl('Limitless_PaypalStaticImage::images/paypalstatic.gif');
+        $fileSource = 'Limitless_PaypalStaticImage::images/'.$locale.'/paypalstatic.gif';
+        $viewSource = null;
+        if ($this->paypalImageExists($fileSource))
+        {
+            $viewSource = $this->template->getViewFileUrl($fileSource);
+        }
+
+        return $viewSource;
+    }
+
+    private function paypalImageExists($source)
+    {
+        $params = [
+            'area' => 'frontend'
+        ];
+        $asset = $this->assetRepository->createAsset($source, $params);
+        try {
+            $asset->getSourceFile();
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 
     private function getStaticPaypalImageEnabled()
