53918f50aba425d52339de552e2bc0cb5901d7aa
Bev Mitchell <bev.mitchell@trueshopping.co.uk>
2017-10-17 13:43:01 +0100

    TP11784 override broken M2 tracking links in MyAccount (#338)
    * tracking link fix
    
    * tracking link fix
    
    * add config
    
    * composer

--- limitless/config.php
+++ limitless/config.php
@@ -231,6 +231,7 @@ return array (
             'Limitless_WorldpaySelectForm' => 1,
             'Limitless_InlineTranslationFix' => 1,
             'Limitless_Reports' => 1,
+            'Limitless_TrackingLink' => 1,
             'Limitless_Product_PDF' => 1,
             'Limitless_ProductOOSSortOrder' => 1,
             'Limitless_ContactUsPopUp' => 1,
